1e87bb856d44d460e37fc902dd8a5c11654255df
Vsevolod-pop <vopopenko@gmail.com>
2025-04-24 05:09:21 +0300

    Добавьте файлы проекта.

---
+++ LW6_task1/LW6_task1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <C:/Users/Сева/source/repos/StructuraFunction/StructuraFunction/StructuraFunction.cpp>
+#include "C:\Users\Сева\source\repos\Struktura\Struktura\Struktura.h"
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	University a = CreatUniversity();
+	University b = CreatUniversitySorted(a);
+	CoutUniversity(a);
+	cout << "__________________________________" << endl << "-----------------------------" << endl;
+	CoutUniversity(b);
+}
+
+
